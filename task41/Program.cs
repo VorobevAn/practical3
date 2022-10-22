8b2e7f5216a3533c57fa7cac3995cbc9fc454911
Vorodev <cust89@mail.ru>
2022-10-22 16:06:52 +0500

    finish

---
+++ task41/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Main();
+
+void Main()
+{
+    Console.Write("Введите чила через запятую: ");
+    string str = Console.ReadLine()!;
+
+    int length = ArrayLength(str);
+    
+    int[] numbers = new int[length]; 
+
+    int index = 0;
+    for (int i = 0; i < str.Length; i++)         // исключаем из введеной строки запятые
+    {
+        string temp = "";
+
+        while (str[i] != ',')
+        {
+            if (i != str.Length - 1)
+            {
+                temp += str[i]; i++;
+            }
+            else
+            {
+                temp += str[i]; break;
+            }
+        }
+        numbers[index] = int.Parse(temp);
+        index++;
+    }
+    Print(numbers);
+    PositiveNumbers(numbers);
+}
+int ArrayLength(string s)   // Находим длинну массива
+{
+    int caunt = 1;
+    for (int i = 0; i < s.Length; i++)
+    {
+       if (s[i] == ',')
+      {
+         caunt ++;
+      }
+    }
+    return caunt;
+}
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void PositiveNumbers(int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el > 0)
+        {
+            count++;
+
+        }
+    }
+    Console.WriteLine($"Количество чисел больше нуля = {count}");
+}
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Введите начальную точку первой прямой b1: ");
+    double b1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите конечную точку первой прямой k1: ");
+    double k1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите начальную точку второй прямой b2: ");
+    double b2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите конечную точку второй прямой k2: ");
+    double k2 = double.Parse(Console.ReadLine());
+    Points(k1, b1, k2, b2);
+}
+
+
+void Points(double K1, double B1, double K2, double B2)
+{
+    if (K1 == K2)
+    {
+        Console.WriteLine("Прямые паралельны");
+    }
+    else
+    {
+        double x = (B2 - B1) / (K1 - K2);
+        Console.WriteLine($"x = {x}");
+
+        double y = K1 * x + B1;
+        Console.WriteLine($"y = {y}");
+    }
+}
